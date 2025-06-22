a838f3e6e2223d72572c1c00ad585fac56f13d6f
Mukund-9 <mukundamitsharma@gmail.com>
2025-06-22 20:51:21 +0530

    Completed project 3

--- project3/index.js
+++ project3/index.js
@@ -2,13 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns=require('dns');
+const urlParser = require('url');
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const mongoose=require('mongoose');
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+// Basic Configuration
+const port = process.env.PORT || 3000;
+const urlSchema= mongoose.Schema({
+  original_url:String,
+  short_url:Number
+});
+const url=new mongoose.model("url",urlSchema);
+app.use(express.urlencoded({ extended: false }));
+let counter=1;
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +29,26 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl',async(req,res)=>{
+  const originalUrl= req.body.url;
+  const hostname=urlParser.parse(originalUrl).hostname;
+  dns.lookup(hostname,async(err)=>{
+    if (err) return res.json({ error: 'invalid url' });
+    let existing= await url.findOne({original_url : originalUrl});
+    if(existing) return res.json(existing);
+    const newUrl=new url({original_url : originalUrl,short_url: counter});
+    counter++;
+    await newUrl.save();
+    res.json(newUrl);
+  });
+});
+app.get('/api/shorturl/:short',async(req,res)=>{
+  const urlEntry=await url.findOne({short_url : req.params.short});
+  if(!urlEntry) return res.json({error : 'No short URL found'});
+  res.redirect(urlEntry.original_url);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
